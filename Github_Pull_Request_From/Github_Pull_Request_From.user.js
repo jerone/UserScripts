4648caf7f0276c93c787aa07bbc07b54866923ce
jerone <jeronevw@hotmail.com>
2014-02-13 09:46:29 +0100

    https://github.com/jerone/UserScripts/blob/master/Github_Pull_Request_From/Github_Pull_Request_From.user.js

--- Github_Pull_Request_From/Github_Pull_Request_From.user.js
+++ Github_Pull_Request_From/Github_Pull_Request_From.user.js
@@ -1,25 +1,26 @@
 // ==UserScript==
-// @name        Github Pull Request From Link
-// @namespace   https://github.com/jerone/UserScripts
-// @description Make pull request original branch linkable
-// @author      jerone
-// @homepageUrl https://github.com/jerone/UserScripts
-// @include     *://github.com/*/*/pull/*
-// @version     1
-// @grant       none
+// @name         Github Pull Request From Link
+// @namespace    https://github.com/jerone/UserScripts
+// @description  Make pull request original branch linkable
+// @author       jerone
+// @homepageUrl  https://github.com/jerone/UserScripts
+// @include      *://github.com/*/*/pull/*
+// @version      2
+// @grant        none
+// @contribution Changes based on Firefox extension https://github.com/diegocr/GitHubExtIns by https://github.com/diegocr
 // ==/UserScript==
 (function () {
-    var targetTreeSpan = document.querySelectorAll('span.commit-ref.current-branch.css-truncate.js-selectable-text.expandable') [1];
-    var branchTree = targetTreeSpan.textContent.trim();
-    var splitTree = branchTree.indexOf(':');
-    var urlTree = [
-        '//github.com',
-        branchTree.substring(0, splitTree),
-        document.querySelector('.js-current-repository') .textContent,
-        'tree',
-        branchTree.substring(splitTree + 1, branchTree.length)
-    ].join('/');
-    var targetSpanA = document.createElement('a');
+    var targetTreeSpan = document.querySelectorAll('span.commit-ref.current-branch.css-truncate.js-selectable-text.expandable') [1],
+        branchTree = targetTreeSpan.textContent.trim() .split(':'),
+        userTree = branchTree.shift(),
+        urlTree = [
+            '//github.com',
+            userTree,
+            document.querySelector('.js-current-repository') .textContent,
+            'tree',
+            branchTree.join(':')
+        ].join('/'),
+        targetSpanA = document.createElement('a');
     targetSpanA.setAttribute('href', urlTree);
     targetSpanA.innerHTML = targetTreeSpan.innerHTML;
     targetTreeSpan.innerHTML = '';
