cb2a752d43be8030428a22b048ad0500d09e03fd
jerone <jeronevw@hotmail.com>
2014-02-24 23:33:50 +0100

    New metadata

--- Horizon_TV_Fixer/155147.user.js
+++ Horizon_TV_Fixer/155147.user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name        Horizon TV Fixer
-// @namespace   https://userscripts.org/scripts/show/155147
-// @description Horizon TV Fixer
+// @namespace   https://github.com/jerone/UserScripts/
+// @description Fixes some issues & styles and adds some new features on Horizon TV Gids.
+// @homepage    https://github.com/jerone/UserScripts/tree/master/Horizon_TV_Fixer/
+// @homepageURL https://github.com/jerone/UserScripts/tree/master/Horizon_TV_Fixer/
+// @downloadURL https://github.com/jerone/UserScripts/raw/master/Horizon_TV_Fixer/155147.user.js
 // @include     *horizon.tv*
-// @version     17
-// @grant		GM_addStyle
+// @version     18
+// @grant       GM_addStyle
 // ==/UserScript==
 
 (function HorizonTVFixer(){
 
-    console.log("Version: " + unsafeWindow.BBVSettingsObject.version.major + "." + unsafeWindow.BBVSettingsObject.version.minor + "." + unsafeWindow.BBVSettingsObject.version.micro);
+	console.log("Version: " + unsafeWindow.BBVSettingsObject.version.major + "." + unsafeWindow.BBVSettingsObject.version.minor + "." + unsafeWindow.BBVSettingsObject.version.micro);
 
 	/* Social share & Program/Film info; */
 	var url = location.href,
@@ -26,13 +29,13 @@
 				},
 				icon: "http://www.imdb.com/favicon.ico"
 			},
-            YouTube: {
+			YouTube: {
 				submit: function(title, subtitle, channel, time){
 					return "http://www.youtube.com/results?search_query=" + encodeURIComponent((title + (subtitle ? " - " + subtitle : "")).trim());
 				},
 				icon: "http://www.youtube.com/favicon.ico"
 			},
-            Twitter: {
+			Twitter: {
 				submit: function(title, subtitle, channel, time){
 					return "https://twitter.com/intent/tweet?original_referer=" + encodeURIComponent(url) + 
 								"&source=tweetbutton&url=" + encodeURIComponent(url) + 
@@ -52,66 +55,66 @@
 				},
 				icon: "http://www.kijkwijzer.nl/favicon.ico"
 			},
-            IPTorrents: {
+			IPTorrents: {
 				submit: function(title, subtitle, channel, time){
 					return "http://iptorrents.com/torrents?q=" + encodeURIComponent(title.trim());
 				},
 				icon: "http://iptorrents.com/favicon.ico"
 			}
 		};
-    var _orion_modules_EPG_ListingsView_prototype_showDetails = unsafeWindow.orion.modules.EPG.ListingsView.prototype.showDetails;  // https://www.horizon.tv/etc/designs/orion/upc/js/orion/modules/EPG/ListingsView.js?v=34
-    unsafeWindow.orion.modules.EPG.ListingsView.prototype.showDetails = function(imi){
-        _orion_modules_EPG_ListingsView_prototype_showDetails.apply(this, arguments);  // execute original code;
+	var _orion_modules_EPG_ListingsView_prototype_showDetails = unsafeWindow.orion.modules.EPG.ListingsView.prototype.showDetails;  // https://www.horizon.tv/etc/designs/orion/upc/js/orion/modules/EPG/ListingsView.js?v=34
+	unsafeWindow.orion.modules.EPG.ListingsView.prototype.showDetails = function(imi){
+		_orion_modules_EPG_ListingsView_prototype_showDetails.apply(this, arguments);  // execute original code;
 
-        var $listing = unsafeWindow.$('.listing[data-imi="' + imi + '"]'),
-        	$channel = $listing.closest('.channel-listing'),
-        	station = $channel.get(0),
-        	wrap = station.nextSibling;
-        if(wrap.classList.contains("done-social")) return;  // ignore clicking multiple times on the same program;
-        wrap.classList.add("done-social");
-        var details = wrap.querySelector(".details"),
-            title = details.querySelector("h3").textContent,
-            subtitle = (details.querySelector("h4") || { textContent: "" }).textContent,
-            channel = details.querySelector(".channel-details").textContent.split(", ");
-        var messageDiv = document.createElement("div");
-        messageDiv.classList.add("message");
-        messageDiv.appendChild(document.createElement("span"));
-        messageDiv.style.bottom = "10px";
-        messageDiv.style.top = messageDiv.style.width = "auto";
-        var messageP = document.createElement("p");
-        messageDiv.appendChild(messageP);
-        details.appendChild(messageDiv);
-        for(var key in socials){
-            var social = socials[key],
-                socialA = document.createElement("a"),
-                socialImg = document.createElement("img"),
-                submit = social.submit(title, subtitle, channel[0], channel[1]);
-            messageP.appendChild(socialA);
-            socialA.appendChild(socialImg);
-            socialA.href = submit;
-            socialA.target = "_blank";
-            socialA.style.display = "inline-block";
-            socialA.style.margin = "2px 2px 0 2px";
-            socialImg.src = social.icon;
-            socialImg.style.height = socialImg.style.width = "16px";
-            socialImg.title = "[" + key + "] " + submit;
-        }
+		var $listing = unsafeWindow.$('.listing[data-imi="' + imi + '"]'),
+			$channel = $listing.closest('.channel-listing'),
+			station = $channel.get(0),
+			wrap = station.nextSibling;
+		if(wrap.classList.contains("done-social")) return;  // ignore clicking multiple times on the same program;
+		wrap.classList.add("done-social");
+		var details = wrap.querySelector(".details"),
+			title = details.querySelector("h3").textContent,
+			subtitle = (details.querySelector("h4") || { textContent: "" }).textContent,
+			channel = details.querySelector(".channel-details").textContent.split(", ");
+		var messageDiv = document.createElement("div");
+		messageDiv.classList.add("message");
+		messageDiv.appendChild(document.createElement("span"));
+		messageDiv.style.bottom = "10px";
+		messageDiv.style.top = messageDiv.style.width = "auto";
+		var messageP = document.createElement("p");
+		messageDiv.appendChild(messageP);
+		details.appendChild(messageDiv);
+		for(var key in socials){
+			var social = socials[key],
+				socialA = document.createElement("a"),
+				socialImg = document.createElement("img"),
+				submit = social.submit(title, subtitle, channel[0], channel[1]);
+			messageP.appendChild(socialA);
+			socialA.appendChild(socialImg);
+			socialA.href = submit;
+			socialA.target = "_blank";
+			socialA.style.display = "inline-block";
+			socialA.style.margin = "2px 2px 0 2px";
+			socialImg.src = social.icon;
+			socialImg.style.height = socialImg.style.width = "16px";
+			socialImg.title = "[" + key + "] " + submit;
+		}
 	};
 
-	
+
 	/* Tooltips; */
 	ForEachListing("tooltip", (listing) => {
 		listing.title = listing.querySelector(".title").textContent;
 	});
-	
-	
+
+
 	/* Live/HD green square explanation is removed by CSS fixes below, so we add it back; */
 	PageLoad(function(){
 		Array.forEach(document.querySelectorAll(".live-hd"), (live) => {
 			live.title = "Kijk LIVE";
 		});
 	});
-	
+
 	/* Style fixes; */
 	GM_addStyle(
 /* removed white header; */								"\
@@ -165,17 +168,17 @@
 
 /* hide bottom bar; */									"\
 .MyOrionBar  {											\
-    display: none;										\
+	display: none;										\
 }														"+
 	"");
-	
+
 	function PageLoad(done){
 		if(unsafeWindow.$){
 			//unsafeWindow.$(function(){console.log("events: ", unsafeWindow.$(".channel-guide").data("events") );});
 			unsafeWindow.$(".channel-guide").on("loaded", done);  // only on-page jQuery can catch this event;
 		}
 	}
-	
+
 	function ForEachListing(name, done){
 		PageLoad(function(){
 			var listings = document.querySelectorAll(".listing:not(.done-"+name+")");  // get all listings that don't have been processed yet;
@@ -186,6 +189,6 @@
 		});
 	}
 
-    //unsafeWindow.orion.services.LinearDataService.getActiveListing(unsafeWindow.$(this).data('imi')).done(function(){console.log("test");});
+	//unsafeWindow.orion.services.LinearDataService.getActiveListing(unsafeWindow.$(this).data('imi')).done(function(){console.log("test");});
 
 })();
