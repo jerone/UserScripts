8f2be2d972f755794750e18cc48cdc9e5c4cec91
jerone <jeronevw@hotmail.com>
2014-02-27 15:17:51 +0100

    Added Github_Commit_Diff userscript

---
+++ Github_Commit_Diff/Github_Commit_Diff.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        Github Commit Diff
+// @namespace   https://github.com/jerone/UserScripts
+// @description Adds button to show diff (or patch) file for commit
+// @author      jerone
+// @homepage    https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff
+// @homepageURL https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff
+// @downloadURL https://github.com/jerone/UserScripts/raw/master/Github_Commit_Diff/Github_Commit_Diff.user.js
+// @include     http*://github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function(){
+
+    function addButton() {
+        if(!/\/commit\//.test(location.href) || !document.querySelector(".explain")) return;
+        
+        var r;
+        if((r = document.querySelector(".GithubCommitDiffButton"))) r.parentElement.removeChild(r);
+        
+        function getPatchOrDiffHref(type){
+            return (document.querySelector("link[type='text/plain+" + type + "']") 
+               || { href: location.href + "." + type }).href;
+        };
+        
+        var b = document.querySelector(".explain .minibutton");
+           
+        var s = document.createElement("span");
+        s.textContent = " ";
+        s.classList.add("octicon", "octicon-diff");
+        s.style.color = "#333";  // set color because of css selector `p.explain .octicon`;
+        
+        var a = document.createElement("a");
+        a.classList.add("GithubCommitDiffButton", "minibutton", "tooltipped", "tooltipped-s");
+        a.setAttribute("href", getPatchOrDiffHref("diff"));
+        a.setAttribute("title", "Show commit diff.\r\nHold Shift to open commit patch.");
+        a.setAttribute("rel", "nofollow");
+        a.setAttribute("aria-label", a.getAttribute("title"));
+        a.style.marginLeft = "10px";  // give us some room;
+        a.appendChild(s);
+        a.appendChild(document.createTextNode("Diff"));
+    
+        b.parentNode.insertBefore(a, b);
+        
+        a.addEventListener("click", function(e){
+            if(e.shiftKey) {
+                e.preventDefault();
+                location.href = getPatchOrDiffHref("patch");
+            }
+        }, false);
+    }
+    
+    // init;
+    addButton();
+    
+    // on pjax;
+    $(document).on('pjax:success', addButton);
+
+})();
---
+++ Github_Commit_Diff/TEMPLATE.md
@@ -0,0 +1,25 @@
+# [Github Commit Diff](https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff)
+
+[![Install](https://raw.github.com/jerone/UserScripts/master/_resources/Install-button.jpg)](https://github.com/jerone/UserScripts/raw/master/Github_Commit_Diff/Github_Commit_Diff.user.js)
+
+### Description
+
+Adds button to show diff file for commit. Hold Shift key to open the patch file.
+
+### Screenshot
+
+ -
+
+### Compatible
+
+* [![](https://raw.github.com/jerone/UserScripts/master/_resources/Greasemonkey.png) Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/Firefox.png) Mozilla Firefox Desktop](http://www.mozilla.org/en-US/firefox/fx/#desktop).
+
+<sub>Please [notify](https://github.com/jerone/UserScripts/issues/new?title=Userscript%20%3Cname%3E%20%28%3Cversion%3E%29%20also%20works%20in%20%3Cbrowser%3E%20on%20%3Cdesktop/device%3E) when this userscript is successfully tested in another browser...</sub>
+
+### Notes
+
+ -
+
+### External links
+
+* [Greasy Fork](https://greasyfork.org/scripts/77)
