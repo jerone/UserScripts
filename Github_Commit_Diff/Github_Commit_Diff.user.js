15fce33efbb7ed0d817ba2fb870811c4d2ecb7bb
jerone <jeronevw@hotmail.com>
2014-03-10 01:52:36 +0100

    Updated Github Commit Diff

--- Github_Commit_Diff/Github_Commit_Diff.user.js
+++ Github_Commit_Diff/Github_Commit_Diff.user.js
@@ -6,55 +6,56 @@
 // @homepage    https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff
 // @homepageURL https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff
 // @downloadURL https://github.com/jerone/UserScripts/raw/master/Github_Commit_Diff/Github_Commit_Diff.user.js
-// @include     http*://github.com/*
-// @version     1
+// @include     https://github.com/*
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
-(function(){
+(function() {
 
-    function addButton() {
-        if(!/\/commit\//.test(location.href) || !document.querySelector(".explain")) return;
-        
-        var r;
-        if((r = document.querySelector(".GithubCommitDiffButton"))) r.parentElement.removeChild(r);
-        
-        function getPatchOrDiffHref(type){
-            return (document.querySelector("link[type='text/plain+" + type + "']") 
-               || { href: location.href + "." + type }).href;
-        };
-        
-        var b = document.querySelector(".explain .minibutton");
-           
-        var s = document.createElement("span");
-        s.textContent = " ";
-        s.classList.add("octicon", "octicon-diff");
-        s.style.color = "#333";  // set color because of css selector `p.explain .octicon`;
-        
-        var a = document.createElement("a");
-        a.classList.add("GithubCommitDiffButton", "minibutton", "tooltipped", "tooltipped-s");
-        a.setAttribute("href", getPatchOrDiffHref("diff"));
-        a.setAttribute("title", "Show commit diff.\r\nHold Shift to open commit patch.");
-        a.setAttribute("rel", "nofollow");
-        a.setAttribute("aria-label", a.getAttribute("title"));
-        a.style.marginLeft = "10px";  // give us some room;
-        a.appendChild(s);
-        a.appendChild(document.createTextNode("Diff"));
-    
-        b.parentNode.insertBefore(a, b);
-        
-        a.addEventListener("click", function(e){
-            if(e.shiftKey) {
-                e.preventDefault();
-                location.href = getPatchOrDiffHref("patch");
-            }
-        }, false);
-    }
-    
-    // init;
-    addButton();
-    
-    // on pjax;
-    $(document).on('pjax:success', addButton);
+	function addButton() {
+		var e;
+		if (!/\/commit\//.test(location.href) || !(e = document.querySelector(".explain"))) return;
+
+		var r = e.querySelector(".GithubCommitWhitespaceButton");
+		if (r) r.parentElement.removeChild(r);
+
+		function getPatchOrDiffHref(type) {
+			return (document.querySelector("link[type='text/plain+" + type + "']")
+				|| { href: location.href + "." + type }).href;
+		};
+
+		var b = e.querySelector(".minibutton");
+
+		var s = document.createElement("span");
+		s.textContent = " ";
+		s.classList.add("octicon", "octicon-diff");
+		s.style.color = "#333";  // set color because of css selector `p.explain .octicon`;
+
+		var a = document.createElement("a");
+		a.classList.add("GithubCommitDiffButton", "minibutton", "tooltipped", "tooltipped-s");
+		a.setAttribute("href", getPatchOrDiffHref("diff"));
+		a.setAttribute("title", "Show commit diff.\r\nHold Shift to open commit patch.");
+		a.setAttribute("rel", "nofollow");
+		a.setAttribute("aria-label", a.getAttribute("title"));
+		a.style.marginLeft = "10px";  // give us some room;
+		a.appendChild(s);
+		a.appendChild(document.createTextNode("Diff"));
+
+		b.parentNode.insertBefore(a, b);
+
+		a.addEventListener("click", function(e) {
+			if (e.shiftKey) {
+				e.preventDefault();
+				location.href = getPatchOrDiffHref("patch");
+			}
+		}, false);
+	}
+
+	// init;
+	addButton();
+
+	// on pjax;
+	$(document).on('pjax:success', addButton);
 
 })();
--- Github_Commit_Diff/README.md
+++ Github_Commit_Diff/README.md
@@ -8,13 +8,22 @@ Adds button to show diff file for commit. Hold Shift key to open the patch file.
 
 ### Screenshot
 
- -
+![Github Commit Diff screenshot](https://github.com/jerone/UserScripts/tree/master/Github_Commit_Diff/Github_Commit_Diff/screenshot.jpg)
 
 ### Compatible
 
 * [![](https://raw.github.com/jerone/UserScripts/master/_resources/Greasemonkey.png) Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/Firefox.png) Mozilla Firefox Desktop](http://www.mozilla.org/en-US/firefox/fx/#desktop).
 
 <sub>Please [notify](https://github.com/jerone/UserScripts/issues/new?title=Userscript%20%3Cname%3E%20%28%3Cversion%3E%29%20also%20works%20in%20%3Cbrowser%3E%20on%20%3Cdesktop/device%3E) when this userscript is successfully tested in another browser...</sub>
+
+### Version History
+
+<dl>
+    <dt>1.1</dt>
+        <dd>Clean up</dd>
+    <dt>1.0</dt>
+        <dd>Initial version</dd>
+</dl> 
 
 ### Notes
 
