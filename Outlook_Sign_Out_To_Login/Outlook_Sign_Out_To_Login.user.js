2979d1a0643475ec7967579506da88562d81f533
jerone <jeronevw@hotmail.com>
2014-04-09 12:43:55 +0200

    Added "Outlook Sign Out To Login" userscript;

---
+++ Outlook_Sign_Out_To_Login/Outlook_Sign_Out_To_Login.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        Outlook Sign Out To Login
+// @namespace   https://github.com/jerone/UserScripts
+// @description Redirect back to login page when signing out from Outlook
+// @author      jerone
+// @homepage    https://github.com/jerone/UserScripts/tree/master/Outlook_Sign_Out_To_Login
+// @homepageURL https://github.com/jerone/UserScripts/tree/master/Outlook_Sign_Out_To_Login
+// @downloadURL https://github.com/jerone/UserScripts/raw/master/Outlook_Sign_Out_To_Login/Outlook_Sign_Out_To_Login.user.js
+// @updateURL   https://github.com/jerone/UserScripts/raw/master/Outlook_Sign_Out_To_Login/Outlook_Sign_Out_To_Login.user.js
+// @include     *://*.mail.live.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+	document.getElementById("c_signout").href = "https://login.live.com/logout.srf?id=64855";
+})();
