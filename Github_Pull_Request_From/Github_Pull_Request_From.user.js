9435768300f6b3c2f10ff6240f53319d630916a9
Jeroen van Warmerdam <jeronevw@hotmail.com>
2014-08-31 00:55:31 +0200

    * Fixed issues after recent layout updates; * Added native & TamperMonkey for Google Chrome compatibility;

--- Github_Pull_Request_From/Github_Pull_Request_From.user.js
+++ Github_Pull_Request_From/Github_Pull_Request_From.user.js
@@ -1,32 +1,47 @@
 // ==UserScript==
-// @name         Github Pull Request From Link
-// @namespace    https://github.com/jerone/UserScripts/
-// @description  Make pull request original branch linkable
-// @author       jerone
-// @copyright    2014+, jerone (http://jeroenvanwarmerdam.nl)
-// @license      GNU GPLv3
-// @homepage     https://github.com/jerone/UserScripts/tree/master/Github_Pull_Request_From
-// @homepageURL  https://github.com/jerone/UserScripts/tree/master/Github_Pull_Request_From
-// @downloadURL  https://github.com/jerone/UserScripts/raw/master/Github_Pull_Request_From/Github_Pull_Request_From.user.js
-// @include      *://github.com/*/*/pull/*
-// @version      10
-// @grant        none
-// @contribution Changes based on Firefox extension https://github.com/diegocr/GitHubExtIns by https://github.com/diegocr
+// @name        Github Pull Request From Link
+// @namespace   https://github.com/jerone/UserScripts/
+// @description Make pull request original branch linkable
+// @author      jerone
+// @copyright   2014+, jerone (http://jeroenvanwarmerdam.nl)
+// @license     GNU GPLv3
+// @homepage    https://github.com/jerone/UserScripts/tree/master/Github_Pull_Request_From
+// @homepageURL https://github.com/jerone/UserScripts/tree/master/Github_Pull_Request_From
+// @downloadURL https://github.com/jerone/UserScripts/raw/master/Github_Pull_Request_From/Github_Pull_Request_From.user.js
+// @updateURL   https://github.com/jerone/UserScripts/raw/master/Github_Pull_Request_From/Github_Pull_Request_From.user.js
+// @version     11
+// @grant       none
+// @include     https://github.com/*/*
 // ==/UserScript==
-(function(){
-    var targetTreeSpan = document.querySelectorAll('span.commit-ref.current-branch.css-truncate.js-selectable-text.expandable')[1],
-        branchTree = targetTreeSpan.textContent.trim().split(':'),
-        userTree = branchTree.shift(),
-        urlTree = [
-            '//github.com',
-            userTree,
-            document.querySelector('.js-current-repository').textContent,
-            'tree',
-            branchTree.join(':')
-        ].join('/'),
-        targetTreeA = document.createElement('a');
-    targetTreeA.setAttribute('href', urlTree);
-    targetTreeA.innerHTML = targetTreeSpan.innerHTML;
-    targetTreeSpan.innerHTML = '';
-    targetTreeSpan.appendChild(targetTreeA);
-})();
+/* global unsafeWindow */
+
+(function(unsafeWindow) {
+
+	String.format = function(string) {
+		var args = Array.prototype.slice.call(arguments, 1, arguments.length);
+		return string.replace(/{(\d+)}/g, function(match, number) {
+			return typeof args[number] !== "undefined" ? args[number] : match;
+		});
+	};
+
+	// init;
+	function init() {
+		var repo = document.querySelector(".js-current-repository").textContent;
+		Array.prototype.forEach.call(document.querySelectorAll("span.commit-ref.current-branch"), function(treeSpan) {
+			var tree = treeSpan.textContent.trim().split(":");
+			var treeLink = document.createElement("a");
+			treeLink.setAttribute("href", String.format("https://github.com/{0}/{1}/tree/{2}",
+											tree.shift(),		// user;
+											repo,				// repository;
+											tree.join(":")));	// branch;
+			treeLink.innerHTML = treeSpan.innerHTML;
+			treeSpan.innerHTML = "";
+			treeSpan.appendChild(treeLink);
+		});
+	}
+	init();
+
+	// on pjax;
+	unsafeWindow.$(document).on("pjax:end", init);  // `pjax:end` also runs on history back;
+
+})(typeof unsafeWindow !== "undefined" ? unsafeWindow : window);
--- Github_Pull_Request_From/README.md
+++ Github_Pull_Request_From/README.md
@@ -14,19 +14,31 @@ Make pull request original branch linkable.
 
 * [![](https://raw.github.com/jerone/UserScripts/master/_resources/Greasemonkey.png) Greasemonkey](https://addons.mozilla.org/firefox/addon/greasemonkey/) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/Firefox.png) Mozilla Firefox Desktop](http://www.mozilla.org/en-US/firefox/fx/#desktop).
 * [![](https://raw.github.com/jerone/UserScripts/master/_resources/Scriptish.png) Scriptish](https://addons.mozilla.org/firefox/addon/scriptish/) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/Firefox.png) Mozilla Firefox Desktop](http://www.mozilla.org/en-US/firefox/fx/#desktop).
+* [![](https://raw.github.com/jerone/UserScripts/master/_resources/Chromium.png) Native](http://www.chromium.org/developers/design-documents/user-scripts) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/GoogleChrome.png) Google Chrome Desktop](https://www.google.com/chrome/).
+* [![](https://raw.github.com/jerone/UserScripts/master/_resources/Tampermonkey.png) TamperMonkey](http://tampermonkey.net) on [![](https://raw.github.com/jerone/UserScripts/master/_resources/GoogleChrome.png) Google Chrome Desktop](https://www.google.com/chrome/).
 
 <sub>Please [notify](https://github.com/jerone/UserScripts/issues/new?title=Userscript%20%3Cname%3E%20%28%3Cversion%3E%29%20also%20works%20in%20%3Cbrowser%3E%20on%20%3Cdesktop/device%3E) when this userscript is successfully tested in another browser...</sub>
 
 ### Version History
 
-* **10.0**
+* **11**
+    * Fixed issues after recent layout updates;
+    * Added native & TamperMonkey for Google Chrome compatibility;
+* **10**
     * Initial version;
 
-### External links
+### Notes
 
-* [Greasy Fork](https://greasyfork.org/scripts/64)
-* [MonkeyGuts](https://monkeyguts.com/code.php?id=96)
+Test cases:
+
+- https://github.com/jerone/UserScripts/pull/12 (4 records);
 
 ### Contributions
 
 * Changes based on Firefox extension [GitHubExtIns](https://github.com/diegocr/GitHubExtIns) by [Diego Casorran](https://github.com/diegocr).
+
+### External links
+
+* [Greasy Fork](https://greasyfork.org/scripts/64)
+* [OpenUserJS](https://openuserjs.org/scripts/jerone/Github_Pull_Request_From_Link)
+* [MonkeyGuts](https://monkeyguts.com/code.php?id=96)
