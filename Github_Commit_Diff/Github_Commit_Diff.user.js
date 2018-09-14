c79eb37b27ed59581607decf79806563d75d43b3
Florian Bruhin <me@the-compiler.org>
2018-09-14 15:49:26 +0200

    :bug: Removed UTF-8 BOM from scripts (#136)
    Closes #135

--- Github_Commit_Diff/Github_Commit_Diff.user.js
+++ Github_Commit_Diff/Github_Commit_Diff.user.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name        Github Commit Diff
 // @namespace   https://github.com/jerone/UserScripts
 // @description Adds button to show diff (or patch) file for commit
