e0349d1ac94ad7a9f49b2b43291187e617b58efc
happymx <18810345440@163.com>
2022-03-10 08:51:32 +0700

    Revert "Merge pull request #127 from darkred/patch-1"
    This reverts commit 1e07dbdf87839662032b11031c79ce085ce0cf07, reversing
    changes made to b56965e63d73594d883750de3d31e265ae85d3ec.

--- Github_News_Feed_Filter/Github_News_Feed_Filter.user.js
+++ Github_News_Feed_Filter/Github_News_Feed_Filter.user.js
@@ -308,9 +308,9 @@
 				alert.classList.add('tag_remove');
 			} else if (alert.classList.contains('gollum')) {
 				alert.classList.remove('gollum');
-				if (!!~alert.innerText.indexOf(' created a wiki page in ')) {
+				if (!!~alert.textContent.indexOf(' created the ')) {
 					alert.classList.add('wiki_created');
-				} else if (!!~alert.innerText.indexOf(' edited a wiki page in ')) {
+				} else if (!!~alert.textContent.indexOf(' edited a wiki page in ')) {
 					alert.classList.add('wiki_edited');
 				}
 			}
@@ -325,7 +325,11 @@
 		Array.prototype.map.call(newsContainer.querySelectorAll(':scope > div > .body'), function(alert) {
 			return alert.parentNode;
 		}).forEach(function(alert) {
-			var alertRepo = alert.querySelector(':scope [data-ga-click*="target:repo"]');
+<<<<<<< HEAD
+			var alertRepo = alert.querySelector(':scope .flex-items-baseline > div > [data-ga-click*="target:repo"]');
+=======
+			var alertRepo = alert.querySelector('[data-ga-click*="target:repo"]');
+>>>>>>> parent of 28f0a76 (Update Github_News_Feed_Filter.user.js)
 			if (alertRepo) { // Follow doesn't contain a repo link.
 				var userRepo = alertRepo.textContent;
 				userRepos.add(userRepo);
