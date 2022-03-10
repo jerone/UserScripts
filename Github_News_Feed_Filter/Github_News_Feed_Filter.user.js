bddac4f2886205256fd4c884c5f7c147b1bd57aa
happymx <18810345440@163.com>
2022-03-10 08:47:42 +0700

    Revert "Fix after another layout change"
    This reverts commit 54ae3521e7c9709dec9e00d2dd5f6ec54ae89e7c.

--- Github_News_Feed_Filter/Github_News_Feed_Filter.user.js
+++ Github_News_Feed_Filter/Github_News_Feed_Filter.user.js
@@ -16,7 +16,7 @@
 // @include     https://github.com/?*
 // @include     https://github.com/orgs/*/dashboard
 // @include     https://github.com/orgs/*/dashboard?*
-// @version     8.2.2
+// @version     8.2.1
 // @grant       none
 // ==/UserScript==
 
@@ -327,7 +327,7 @@
 		Array.prototype.map.call(newsContainer.querySelectorAll(':scope > div > .body'), function(alert) {
 			return alert.parentNode;
 		}).forEach(function(alert) {
-			var alertRepo = alert.querySelector(':scope [data-ga-click*="target:repo"]:not([data-ga-click*="target:repositories"])');
+			var alertRepo = alert.querySelector(':scope [data-ga-click*="target:repo"]');
 			if (alertRepo) { // Follow doesn't contain a repo link.
 				var userRepo = alertRepo.textContent;
 				userRepos.add(userRepo);
@@ -369,15 +369,9 @@
 		Array.prototype.map.call(newsContainer.querySelectorAll(':scope > div > .body'), function(alert) {
 			return alert.parentNode;
 		}).forEach(function(alert) {
-			var usernameElms = alert.querySelectorAll(':scope [data-ga-click*="target:actor"]');
-			Array.prototype.find.call(usernameElms, function(usernameElm){
-                var username = usernameElm.textContent;
-                if (username) {
-					alert.classList.add(username);
-                    users.add(username);
-                    return true;
-                }
-            });
+			var username = alert.querySelector(':scope .flex-items-baseline > div > [data-ga-click*="target:actor"]').textContent;
+			alert.classList.add(username);
+			users.add(username);
 		});
 
 		[...users].sort(function(a, b) {
